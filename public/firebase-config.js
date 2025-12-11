2417f337679eb4d0416da4015ac4fd78826d0b66
JaysCoding-hub <jermainenowak33@gmail.com>
2025-12-11 17:53:21 +0100

    Add files via upload

---
+++ public/firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase configuration for compat SDK usage in browser
+// This file is loaded as a normal <script> after the compat SDKs.
+// Do NOT use ES module imports here; keep a plain object and expose it.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBslZQf7Zmzy2sV0jLUPQNTIrVEzPEXrjU",
+  authDomain: "tbet-ba0fb.firebaseapp.com",
+  databaseURL: "https://tbet-ba0fb-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "tbet-ba0fb",
+  storageBucket: "tbet-ba0fb.firebasestorage.app",
+  messagingSenderId: "396881842090",
+  appId: "1:396881842090:web:85f1692ecc61e045bba5d3",
+  measurementId: "G-0RBKKTKHB8"
+};
